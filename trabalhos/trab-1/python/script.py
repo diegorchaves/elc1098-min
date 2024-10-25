7525cb3a48200b2f72522959670398488079b75b
Diego Ribeiro Chaves <drchaves@inf.ufsm.br>
2024-10-25 10:23:22 -0300

    feat: codigo revisado, falta relatorio
    Co-authored-by: Lucas Xavier  <lucasxpaire@users.noreply.github.com>

--- trabalhos/trab-1/python/script.py
+++ trabalhos/trab-1/python/script.py
@@ -1,4 +1,3 @@
-import numpy as np 
 import pandas as pd 
 import json
 from mlxtend.frequent_patterns import apriori, association_rules
@@ -6,42 +5,49 @@ from mlxtend.preprocessing import TransactionEncoder
 
 json_data = pd.read_json("./data/padaria-ajeitada.json")
 json_str = json_data.to_json(orient='records')
-# print(json_data)
+print("JSON data:")
+print(json_data)
 
 # Carregar os dados do JSON
 data = json.loads(json_str)
 
 # Extrair apenas a lista de produtos
 transactions = [item['produtos'] for item in data]
-
-# print(transactions)
+print("Transactions:")
+print(transactions)
 
 # Utilizando TransactionEncoder para transformar os dados
 encoder = TransactionEncoder()
 encoded_array = encoder.fit(transactions).transform(transactions)
 
-# print(encoded_array)
+print("Encoded array:")
+print(encoded_array)
 
 # Criar um DataFrame
 df = pd.DataFrame(encoded_array, columns=encoder.columns_)
-# print(df)
+print("Data frame:")
+print(df)
 
-# Exemplo de uso do algoritmo Apriori
+# Uso do algoritmo Apriori
 frequent_itemsets = apriori(df, min_support=0.1, use_colnames=True)
+print("Frequent itemsets:")
 print(frequent_itemsets)
 
 # Gerar regras de associação
 rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.3)
 
-# print(rules)
+print("Regras gerais:")
+print(rules)
 
 # Filtrar regras 1 para 1
 rules_1_to_1 = rules[(rules['antecedents'].apply(lambda x: len(x) == 1)) & 
                      (rules['consequents'].apply(lambda x: len(x) == 1))]
 
+print("Regras 1 para 1:")
 print(rules_1_to_1)
 
 # Filtrar regras que têm 'doce' como consequente
 rules_with_sweet = rules[rules['consequents'].apply(lambda x: 'Doce' in x)]
 
+print("Regras com doce:")
 print(rules_with_sweet)
